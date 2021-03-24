facdd7e95752ed801d4f37af9a05f2c68754ba9e
YouChen Liu <youchenliu@outlook.com>
2021-03-25 00:04:05 +0800

    passed, but threr is a better solution

--- 190.reverse-bits.cpp
+++ 190.reverse-bits.cpp
@@ -6,14 +6,40 @@
 
 // @lc code=start
 #include <cstdint>
+#include <deque>
 class Solution {
 public:
   uint32_t reverseBits(uint32_t n) {
-    auto ans = n;
-    for (int i = 0; i < 16; i++) {
-      int shiftLeft = 32 - i - 1;
+    // use deque to reverse order, too slow and too much memory
+    /*
+    std::deque<int> deque;
+    for (int i = 0; i < 32; i++) {
+      deque.push_back(n & 0x01);
+      n >>= 1;
     }
-    return 0;
+    uint32_t ans = 0;
+    while (!deque.empty()) {
+      const auto &bit = deque.front();
+      deque.pop_front();
+      ans <<= 1;
+      ans |= bit;
+    }
+    return ans;
+    */
+    uint32_t maskL = 0x80000000;
+    uint32_t maskR = 0x00000001;
+    for (int i = 0; i < 16; i++) {
+      uint32_t combined = maskL | maskR;
+      const auto left = n & maskL;
+      const auto right = n & maskR;
+      if ((left != 0 && right == 0) || (left == 0 && right != 0)) {
+        const auto combined = maskL | maskR;
+        n ^= combined;
+      }
+      maskL >>= 1;
+      maskR <<= 1;
+    }
+    return n;
   }
 };
 // @lc code=end
